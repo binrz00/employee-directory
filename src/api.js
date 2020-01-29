f89d201486b0c9354816b6287f26d776cf074847
Brian Zauner <binrz00@hotmail.com>
2020-01-28 22:59:23 -0600

    got the cards to render, had to steal from react docs

--- src/api.js
+++
@@ -1,3 +0,0 @@
-export const Data = fetch("https://randomuser.me/api/?results=10").then(res => {
-  return res.json();
-});
--- src/components/card.js
+++ src/components/card.js
@@ -1,26 +1,49 @@
 import React from "react";
 import "./card.css";
-import Data from "../api";
-export default function Card() {
-  return (
-    <div className="col-3">
-      <div className="card">
-        <img
-          src={Data.results[0].picture.thumbnail}
-          className="card-img-top"
-          alt="employee pic"
-        />
-        <div className="card-body">
-          <p className="card-text">
-            Name: {Data.results[0].name.first} {Data.results[0].name.last}
-          </p>
-          <p className="card-text">Age: {Data.results[0].dob.age}</p>
-          <p className="card-text">Phone: {Data.results[0].phone}</p>
-          <p className="card-text">Gender: {Data.results[0].gender}</p>
+export default class Card extends React.Component {
+  state = {
+    isLoaded: false,
+    people: null
+  };
+
+  async componentDidMount() {
+    const res = await fetch("https://randomuser.me/api/?results=10");
+    const jsonres = await res.json();
+    this.setState({
+      isLoaded: true,
+      people: jsonres
+    });
+  }
+
+  render() {
+    const { error, isLoaded, people } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="col-3">
+          <div className="card">
+            <img
+              src={people.results[0].picture.thumbnail}
+              className="card-img-top"
+              alt="employee pic"
+            />
+            <div className="card-body">
+              <p className="card-text">
+                Name: {people.results[0].name.first}{" "}
+                {people.results[0].name.last}
+              </p>
+              <p className="card-text">Age: {people.results[0].dob.age}</p>
+              <p className="card-text">Phone: {people.results[0].phone}</p>
+              <p className="card-text">Gender: {people.results[0].gender}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      );
+    }
+  }
 }
 
   
