60dc47cf967c1af2bf1697f68c0a7f8c4fcc3a36
Brian Zauner <binrz00@hotmail.com>
2020-02-07 22:25:02 -0600

    changed to functional components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import Card from "./components/card"
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import Card from "./components/card";
 function App() {
+  const [people, setPeople] = useState([]);
+
+  const getPeople = async function() {
+    const res = await fetch("https://randomuser.me/api/?results=10");
+    const jsonres = await res.json();
+
+    setPeople(jsonres.results);
+  };
+  useEffect(() => {
+    getPeople();
+  }, []);
+
   return (
     <div className="App">
-     <Card/>
+      {people.map(card => (
+        <Card {...card} />
+      ))}
+
+      {/* <Card /> */}
     </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,49 +1,23 @@
 import React from "react";
 import "./card.css";
-export default class Card extends React.Component {
-  state = {
-    isLoaded: false,
-    people: null
-  };
-
-  async componentDidMount() {
-    const res = await fetch("https://randomuser.me/api/?results=10");
-    const jsonres = await res.json();
-    this.setState({
-      isLoaded: true,
-      people: jsonres
-    });
-  }
-
-  render() {
-    const { error, isLoaded, people } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div className="col-3">
-          <div className="card">
-            <img
-              src={people.results[0].picture.thumbnail}
-              className="card-img-top"
-              alt="employee pic"
-            />
-            <div className="card-body">
-              <p className="card-text">
-                Name: {people.results[0].name.first}{" "}
-                {people.results[0].name.last}
-              </p>
-              <p className="card-text">Age: {people.results[0].dob.age}</p>
-              <p className="card-text">Phone: {people.results[0].phone}</p>
-              <p className="card-text">Gender: {people.results[0].gender}</p>
-            </div>
-          </div>
+export default function Card(props) {
+  return (
+    <div className="col-3">
+      <div className="card">
+        <img
+          src={props.picture.large}
+          className="card-img-top"
+          alt="employee pic"
+        />
+        <div className="card-body">
+          <p className="card-text">
+            Name: {props.name.first} {props.name.last}
+          </p>
+          <p className="card-text">Age: {props.dob.age}</p>
+          <p className="card-text">Phone: {props.phone}</p>
+          <p className="card-text">Gender: {props.gender}</p>
         </div>
-      );
-    }
-  }
+      </div>
+    </div>
+  );
 }
-
-  
