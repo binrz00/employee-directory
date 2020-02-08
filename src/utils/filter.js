15c1f49e760ca1b80dd4cd0e4ac55bcde2b89994
Brian Zauner <binrz00@hotmail.com>
2020-02-08 05:09:04 -0600

    added sort function

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -5,12 +5,14 @@ export default function filter(people, limit, age, dispatch) {
     for (let i = 0; i < people.length; i++) {
       if (people[i].dob.age > param) {
         people.splice(i, 1);
+        i=0
       }
     }
   } else {
     for (let i = 0; i < people.length; i++) {
       if (people[i].dob.age < param) {
         people.splice(i, 1);
+        i=0
       }
     }
   }
--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,15 +1,75 @@
-export default function sort(people, value) {
-    switch (value) {
-        case "age":
-          console.log("age");
-          break;
-        case "gender":
-          console.log("gender");
-          break;
-        case "name":
-          console.log("name");
-          break;
-        default:
-          return alert("please select a parameter of age, gender, or name");
+export default function sort(people, param, order, dispatch) {
+  let keys = [];
+  let orderedpeople = [];
+  if (param === "name") {
+    people.map(person => {
+      keys.push(person.name.last);
+      keys.sort();
+    });
+    let z = 0;
+    let y = 0;
+    if (order === "asc") {
+      while (y < 10) {
+        for (let i = 0; people.length > i; i++) {
+          if (keys[z] === people[i].name.last) {
+            orderedpeople.push(people[i]);
+            z = z + 1;
+            i = 0;
+            y++;
+          }
+        }
+      }
+    } else {
+      while (y < 10) {
+        for (let i = 0; people.length > i; i++) {
+          if (keys[z] === people[i].name.last) {
+            orderedpeople.unshift(people[i]);
+            z = z + 1;
+            i = 0;
+            y++;
+          }
+        }
       }
     }
+  }
+
+  if (param === "age") {
+    people.map(person => {
+      keys.push(person.dob.age);
+      keys.sort(function(a, b) {
+        return b - a;
+      });
+    });
+    let z = 0;
+    let y = 0;
+    if (order === "asc") {
+      while (y < 10) {
+        for (let i = 0; people.length > i; i++) {
+          if (keys[z] === people[i].dob.age) {
+            orderedpeople.unshift(people[i]);
+            z = z + 1;
+            i = 0;
+            y++;
+          }
+        }
+      }
+    } else {
+      while (y < 10) {
+        for (let i = 0; people.length > i; i++) {
+          if (keys[z] === people[i].dob.age) {
+            orderedpeople.push(people[i]);
+            z = z + 1;
+            i = 0;
+            y++;
+          }
+        }
+      }
+    }
+  }
+  dispatch({
+    type: "UPDATE_PEOPLE",
+    payload: {
+      people: orderedpeople
+    }
+  });
+}
